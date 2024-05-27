249ae5c7a48faf57952f5b9ee21f17997f057700
Fernando Cardenas <fernando.cardenas01@epn.edu.ec>
2024-05-27 10:38:49 -0500

    update main

--- deber01/zoo/src/main/kotlin/model/zoo.kt
+++ deber01/zoo/src/main/kotlin/model/zoo.kt
@@ -3,9 +3,9 @@ package model
 import java.time.LocalDate
 
 data class Zoologico(
-    val nombre: String,
+    val name: String,
     val locate: String,
     val dateFundation: LocalDate,
-    val caoacityMax: Int,
+    val capacityMax: Int,
     val animals: MutableList<Animal> = mutableListOf()
 )
--- deber01/zoo/src/main/kotlin/zoologico/main.kt
+++ deber01/zoo/src/main/kotlin/zoologico/main.kt
@@ -1,5 +1,24 @@
 package zoologico
 
+import model.Animal
+import model.Zoologico
+import java.time.LocalDate
+
 fun main(){
-    print("Creacion del proyecto Zoo ")
+    //Animals
+    val animal1 = Animal("Pablo","Elefante",22, LocalDate.now(),100.0,)
+    //println(animal1)
+    val animal2 = Animal("Mattew","Bicho",1, LocalDate.now(),5.3,)
+
+
+    //Zoo
+    val zoo1 = Zoologico(
+        name = "ZOO-Fercho",
+        locate = "Quito-Ecuador",
+        dateFundation = LocalDate.of(1970, 1, 1),
+        capacityMax = 100,
+        animals = mutableListOf(animal1, animal2)
+    )
+    println(zoo1)
+    println(zoo1.animals)
 }
