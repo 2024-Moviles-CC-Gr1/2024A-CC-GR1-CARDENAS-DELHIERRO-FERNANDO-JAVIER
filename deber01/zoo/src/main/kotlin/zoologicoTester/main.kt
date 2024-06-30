f899491de02463d824d6bf47c15de6fdcc6c9543
Fernando Cardenas <fernando.cardenas01@epn.edu.ec>
2024-06-30 15:33:28 -0500

    presentation exam

--- deber01/zoo/src/main/kotlin/zoologicoTester/main.kt
+++ deber01/zoo/src/main/kotlin/zoologicoTester/main.kt
@@ -8,22 +8,7 @@ import java.time.LocalDate
 import java.util.*
 
 fun main() {
-    // Animals
-    val animal1 = Animal("Pablo", "Elefante", 22, LocalDate.now(), 100.0, "ZOO-Fercho")
-    val animal2 = Animal("Mattew", "Bicho", 1, LocalDate.now(), 5.3, "ZOO-Fercho")
-
-    // Zoo
-    val zoo1 = Zoologico(
-        name = "ZOO-Fercho",
-        locate = "Quito-Ecuador",
-        dateFundation = LocalDate.of(1970, 1, 1),
-        capacityMax = 100,
-        animals = mutableListOf(animal1, animal2)
-    )
-
-    ZooCRUD.create(zoo1)
-
-    val scanner = Scanner(System.`in`)
+       val scanner = Scanner(System.`in`)
     var option: Int
 
     do {
